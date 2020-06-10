3056bef37f0a4d9039fd5eeef600ddcee81c161c
Bonde005 <55687108+Bonde005@users.noreply.github.com>
2020-06-10 13:20:49 +0200

    Update template-tilmelding.php

--- hestia-child/template-tilmelding.php
+++ hestia-child/template-tilmelding.php
@@ -1,30 +1,20 @@
 <?php
 /**
- * The template for displaying all single posts and attachments.
- *
- * @package Hestia
- * @since Hestia 1.0
+ * Template Name: Tilmelding
  */
-
 get_header();
 
-do_action( 'hestia_before_single_page_wrapper' );
-
+do_action( 'hestia_before_single_post_wrapper' );
 ?>
+
 <div class="<?php echo hestia_layout(); ?>">
-	<?php
-	$class_to_add = '';
-	if ( class_exists( 'WooCommerce', false ) && ! is_cart() ) {
-		$class_to_add = 'blog-post-wrapper';
-	}
-	?>
-	<div class="blog-post <?php esc_attr( $class_to_add ); ?>">
+	<div class="blog-post blog-post-wrapper">
 		<div class="container">
 			<?php
 			if ( have_posts() ) :
 				while ( have_posts() ) :
 					the_post();
-					get_template_part( 'template-parts/content', 'page' );
+					get_template_part( 'template-parts/content', 'single' );
 				endwhile;
 				else :
 					get_template_part( 'template-parts/content', 'none' );
@@ -32,3 +22,14 @@ do_action( 'hestia_before_single_page_wrapper' );
 				?>
 		</div>
 	</div>
+</div>
+<section>
+	<?php include 'stel_udregner.html';?>
+</section>
+<?php
+if ( ! is_singular( 'elementor_library' ) ) {
+	do_action( 'hestia_blog_related_posts' );
+}
+?>
+<div class="footer-wrapper">
+	<?php get_footer(); ?>
